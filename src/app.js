ab927e9d6fc37608f1ca00c6a9e351566c5d0933
Roma <my.muzzz@gmail.com>
2019-12-03 13:17:18 +0200

    fix

--- src/app.js
+++ src/app.js
@@ -2,13 +2,11 @@ import React from 'react';
 import Header from './components/Header'
 import Weather from './components/Weather'
 
-export default class extends React.Component{
-  render () {
-    return (
-      <>
-        <Header/>
-        <Weather/>
-      </>
-    )
-  }
-}
+export default function () {
+  return (
+    <>
+      <Header/>
+      <Weather/>
+    </>
+  )
+};
